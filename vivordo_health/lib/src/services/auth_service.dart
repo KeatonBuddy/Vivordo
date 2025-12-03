66771058e416e193d4935cad713087c14b51d344
fabf28 <ifavour228@gmail.com>
2025-12-03 14:50:21 -0700

    complete email sign in and login without firestore

--- vivordo_health/lib/src/services/auth_service.dart
+++ vivordo_health/lib/src/services/auth_service.dart
@@ -1,30 +1,42 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:vivordo_health/src/utils/toast.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthService {
   //email sign in
   static Future<void> emailSignup({
     required String emailAddress,
     required String password,
+    required BuildContext context,
+    required Widget nextPage,
   }) async {
     try {
+      //add validation for the email and password
+      //successful signup
       final credential = await FirebaseAuth.instance
           .createUserWithEmailAndPassword(
             email: emailAddress,
             password: password,
           );
+      await Future.delayed(const Duration(seconds: 1));
+      if (context.mounted) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (BuildContext context) => nextPage),
+        );
+      }
     } on FirebaseAuthException catch (e) {
+      //basic password validation - should we make it more complicated?
       if (e.code == 'weak-password') {
-        const message = 'The password provided is too weak.';
-        print(message);
+        const message = 'Password should be at least 6 characters';
+        print(e);
         ToastMessages.authMessage(message: message);
-        //what makes password too weak?
       } else if (e.code == 'email-already-in-use') {
         const message = 'The account already exists for that email.';
-        print(message);
+        print(e);
         ToastMessages.authMessage(message: message);
       }
-      //navigate to the next page
       //store user information in firestore
     } catch (e) {
       print(e);
@@ -32,6 +44,41 @@ class AuthService {
   }
 
   //email sign up
+  static Future<void> emailLogin({
+    required String emailAddress,
+    required String password,
+    required BuildContext context,
+    required Widget nextPage,
+  }) async {
+    try {
+      //add validation for the email and password
+      //successful sign in
+      final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: emailAddress,
+        password: password,
+      );
+      await Future.delayed(const Duration(seconds: 1));
+      if (context.mounted) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (BuildContext context) => nextPage),
+        );
+      }
+    } on FirebaseAuthException catch (e) {
+      //invalid credentials
+      print(e);
+      //should i take extra steps to figure out if its specifcally wrong email or password?
+      if (e.code == 'invalid-credential') {
+        const message = 'Invalid email or password';
+        print(e);
+        ToastMessages.authMessage(message: message);
+      }
+      //log any other errors here - crashlytics?
+      //store user information in firestore
+    } catch (e) {
+      print(e); //log this - crashlytics?
+    }
+  }
 
-  //email
+  //email signout
 }
