9bd51482ba69dcfe749ea6cca8ee6b977c66616f
nparmar4 <neha04parmar@gmail.com>
2025-12-08 20:36:52 -0700

    Login and started dashboard

--- vivordo_health/lib/main.dart
+++ vivordo_health/lib/main.dart
@@ -3,6 +3,8 @@ import 'package:vivordo_health/firebase_options.dart';
 import 'package:vivordo_health/src/pages/log_in_demo.dart' show LoginDemo;
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_ai/firebase_ai.dart';
+import 'screens/login.dart';
+import 'screens/dashboard.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,31 +25,16 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Login Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      debugShowCheckedModeBanner: false,
-      home: LoginDemo(),
+      title: 'Vivordo Health',
+      theme: ThemeData(primarySwatch: Colors.purple),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => LoginScreen(),
+        '/dashboard': (context) => DashboardScreen(),
+      },
     );
   }
 }
