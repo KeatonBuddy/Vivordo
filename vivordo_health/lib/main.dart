c521776b9b917ec62e058a3f32fc363344b6b49c
fabf28 <ifavour228@gmail.com>
2026-01-15 19:34:12 -0700

    firebase ai demo

--- vivordo_health/lib/main.dart
+++ vivordo_health/lib/main.dart
@@ -2,11 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:vivordo_health/firebase_options.dart';
 import 'package:vivordo_health/src/pages/log_in_demo.dart' show LoginDemo;
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_ai/firebase_ai.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  final model = FirebaseAI.googleAI().generativeModel(
+    model: 'gemini-2.5-flash',
+  );
 
+  // Provide a prompt that contains text
+  final prompt = [Content.text('Write a story about a magic backpack.')];
+
+  // To generate text output, call generateContent with the text input
+  final response = await model.generateContent(prompt);
+  print(response.text);
   runApp(const MyApp());
 }
 
--- vivordo_health/macos/Flutter/GeneratedPluginRegistrant.swift
+++ vivordo_health/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,7 @@ import Foundation
 
 import cloud_firestore
 import firebase_analytics
+import firebase_app_check
 import firebase_auth
 import firebase_core
 import firebase_crashlytics
@@ -15,6 +16,7 @@ import google_sign_in_ios
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FirebaseAnalyticsPlugin"))
+  FLTFirebaseAppCheckPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAppCheckPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
