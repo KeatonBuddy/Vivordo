19a66528ed26ffa64cddaf0b13ce6e75f52f2077
fabf28 <ifavour228@gmail.com>
2026-02-10 11:52:51 -0700

    add usermodel to the context

--- vivordo_health/lib/main.dart
+++ vivordo_health/lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:vivordo_health/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:vivordo_health/src/models/user_model.dart';
 import 'screens/login_screen.dart';
 import 'screens/signup_screen.dart';
 import 'screens/home_screen.dart';
@@ -10,11 +12,32 @@ import 'screens/home_screen.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(
-    StreamProvider<User?>.value(
-      value: FirebaseAuth.instance.authStateChanges(),
-      initialData: null,
-      child: const MyApp(),
+    MultiProvider(
+      providers: [
+        StreamProvider<User?>(
+          create: (_) => FirebaseAuth.instance.authStateChanges(),
+          initialData: null,
+        ),
+        StreamProvider<UserModel?>(
+          create: (context) {
+            final user = context.read<User?>();
+            if (user == null) return Stream.value(null);
+            return FirebaseFirestore.instance
+                .collection('users')
+                .doc(user.uid)
+                .snapshots()
+                .map((doc) {
+                  final data = doc.data();
+                  if (data == null) return null;
+                  return UserModel.fromMap(data, doc.id);
+                });
+          },
+          initialData: null,
+        ),
+      ],
+      child: MyApp(),
     ),
   );
 }
