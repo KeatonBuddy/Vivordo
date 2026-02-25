09871ae72f4b368002b782373f101a5c7ff41610
fabf28 <ifavour228@gmail.com>
2026-02-24 22:06:27 -0700

    move goals functions to new file

--- vivordo_health/lib/src/services/user_service.dart
+++ vivordo_health/lib/src/services/user_service.dart
@@ -22,55 +22,6 @@ class UserService {
         .set(firestoreUser.toMap());
   }
 
-  //create goal
-  //TODO: update goal - with 1 get goal defoposit
-  //TODO: get goal - get with 1 autoid
-  //TODO: get all goals - query for all
-
-  static Future<void> createGoal({
-    required User theUser,
-    required String userId,
-    required String title,
-    required String status,
-    String? description,
-    String? category,
-    String? targetMetricType,
-    double? targetValue,
-    String? targetUnit,
-    String? direction,
-    String? endDate,
-    String? progressCurrentValue,
-    String? progressCompletionPercent,
-  }) async {
-    Map<String, dynamic>? progress;
-    if (progressCurrentValue != null) {
-      progress = {
-        "currentValue": progressCurrentValue,
-        "completionPercent": progressCompletionPercent,
-        "lastUpdated": FieldValue.serverTimestamp(),
-      };
-    }
-
-    Goals newGoal = Goals(
-      userId: theUser.uid,
-      title: title,
-      description: description,
-      category: category,
-      targetMetricType: targetMetricType,
-      targetValue: targetValue,
-      targetUnit: targetUnit,
-      direction: direction,
-      startDate: FieldValue.serverTimestamp().toString(),
-      endDate: endDate,
-      status: status,
-      progress: progress,
-      createdAt: FieldValue.serverTimestamp(),
-      updatedAt: FieldValue.serverTimestamp(),
-    );
-
-    await FirebaseFirestore.instance.collection('goals').add(newGoal.toMap());
-  }
-
   static Future<void> submitQuestionare({
     required User? user,
     required Map<String, dynamic> userdata,
